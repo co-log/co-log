f437f022fb15bfb9a7b272b2d54bcd820663b6f5
Dmitry Kovanikov <kovanikov@gmail.com>
2018-08-19 13:09:02 +0800

    Fix implementation

--- co-log/src/Colog/Monad.hs
+++ co-log/src/Colog/Monad.hs
@@ -1,20 +1,29 @@
 {-# LANGUAGE ConstraintKinds #-}
+{-# LANGUAGE InstanceSigs    #-}
 
 module Colog.Monad
        ( LoggerT (..)
        , WithLog
        , logMsg
        , withLog
+       , liftLogAction
        , usingLoggerT
        ) where
 
 import Control.Monad.Reader (MonadReader (..), ReaderT)
+import Control.Monad.Trans.Class (MonadTrans (..))
 
 import Colog.Core (HasLog (..), LogAction (..), overLogAction)
 
+{- |
+-}
 newtype LoggerT msg m a = LoggerT
-    { runLoggerT :: ReaderT (LogAction m msg) m a
-    } deriving (Functor, Applicative, Monad, MonadReader (LogAction m msg))
+    { runLoggerT :: ReaderT (LogAction (LoggerT msg m) msg) m a
+    } deriving (Functor, Applicative, Monad, MonadReader (LogAction (LoggerT msg m) msg))
+
+instance MonadTrans (LoggerT msg) where
+    lift :: Monad m => m a -> LoggerT msg m a
+    lift = LoggerT . lift
 
 type WithLog env msg m = (MonadReader env m, HasLog env msg m)
 
@@ -42,16 +51,20 @@ app :: 'WithLog' env 'String' m => m ()
 app = 'withLog' ('cmap' ("app:" ++)) $ __do__
     'logMsg' "First message..."
     'logMsg' "Second message..."
+@
 -}
 withLog :: WithLog env msg m => (LogAction m msg -> LogAction m msg) -> m a -> m a
 withLog = local . overLogAction
+
+liftLogAction :: (Monad m, MonadTrans t) => LogAction m msg -> LogAction (t m) msg
+liftLogAction (LogAction action) = LogAction (lift . action)
 
 {- | Runner for 'LoggerT' monad. Let's consider one simple example of monadic
 action you have:
 
 @
-example :: WithLog env 'String' m => m ()
-example = __do__
+app :: 'WithLog' env 'String' m => m ()
+app = __do__
     'logMsg' "Starting application..."
     'withLog' ('cmap' ("app:" ++)) $ __do__
         'logMsg' "Application started."
@@ -61,7 +74,7 @@ example = __do__
 You can use the following way of running such example:
 
 @
-'usingLoggerT' ('LogAction' 'putStrLn') example
+'usingLoggerT' ('LogAction' 'putStrLn') app
 @
 
 And you will see this output:
@@ -72,5 +85,5 @@ app:Application started.
 app:Application finished.
 @
 -}
-usingLoggerT :: LogAction m msg -> LoggerT msg m a -> m a
-usingLoggerT action =  flip runReaderT action . runLoggerT
+usingLoggerT :: Monad m => LogAction m msg -> LoggerT msg m a -> m a
+usingLoggerT action =  flip runReaderT (liftLogAction action) . runLoggerT
