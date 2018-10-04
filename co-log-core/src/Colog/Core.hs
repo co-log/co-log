1942dc91a0ba0fe2cbaddc676c8d81bf8683db3b
Giovanni Ornaghi <sphaso@gmail.com>
2018-10-04 17:36:28 +0200

    [#46] Move log actions that work with String to Colog.Core.IO (#51)

--- co-log-core/src/Colog/Core.hs
+++ co-log-core/src/Colog/Core.hs
@@ -1,9 +1,11 @@
 module Colog.Core
        ( module Colog.Core.Action
        , module Colog.Core.Class
+       , module Colog.Core.IO
        , module Colog.Core.Severity
        ) where
 
 import Colog.Core.Action
 import Colog.Core.Class
+import Colog.Core.IO
 import Colog.Core.Severity
