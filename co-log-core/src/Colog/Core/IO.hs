2c6d2f674a71e7c51d04d7ae634aa18d1464aaec
lucazulian <info@lucazulian.it>
2018-10-10 20:50:07 +0200

    Add doctests for Colog.Core.IO module

--- co-log-core/src/Colog/Core/IO.hs
+++ co-log-core/src/Colog/Core/IO.hs
@@ -26,7 +26,11 @@ foo
 logStringStderr :: MonadIO m => LogAction m String
 logStringStderr = logStringHandle stderr
 
-{- | Action that prints 'String' to 'Handle'. -}
+{- | Action that prints 'String' to 'Handle'.
+
+>>> (unLogAction . logStringHandle) stderr "foo"
+foo
+-}
 logStringHandle :: MonadIO m => Handle -> LogAction m String
 logStringHandle handle = LogAction $ liftIO . hPutStrLn handle
 
@@ -36,10 +40,18 @@ file only once at the start of the application and write to 'Handle' instead of
 opening file each time we need to write to it.
 
 Opens file in 'AppendMode'.
+>>> logger action = unLogAction action "foo"
+>>> withLogStringFile "/dev/stdout" logger
+foo
 -}
 withLogStringFile :: MonadIO m => FilePath -> (LogAction m String -> IO r) -> IO r
 withLogStringFile path action = withFile path AppendMode $ action . logStringHandle
 
-{- | Lifts a LogAction over IO into a more general Monad. -}
+{- | Lifts a LogAction over IO into a more general Monad.
+
+>>> logToStdout = LogAction putStrLn
+>>> unLogAction (liftLogIO logToStdout) "foo"
+foo
+-}
 liftLogIO :: MonadIO m => LogAction IO msg -> LogAction m msg
 liftLogIO (LogAction action) = LogAction (liftIO . action)
