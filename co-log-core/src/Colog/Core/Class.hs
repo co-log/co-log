47f00097790a596f2641cfc04b7e6c6465154e67
sphaso <sphaso@gmail.com>
2018-11-21 20:13:45 +0100

    added overLogaction to HasLog typeclass

--- co-log-core/src/Colog/Core/Class.hs
+++ co-log-core/src/Colog/Core/Class.hs
@@ -6,7 +6,6 @@
 
 module Colog.Core.Class
        ( HasLog (..)
-       , overLogAction
        ) where
 
 import Colog.Core.Action (LogAction)
@@ -18,11 +17,9 @@ TODO: laws
 class HasLog env msg m where
     getLogAction :: env -> LogAction m msg
     setLogAction :: LogAction m msg -> env -> env
+    overLogAction :: (LogAction m msg -> LogAction m msg) -> env -> env
 
 instance HasLog (LogAction m msg) msg m where
     getLogAction = id
     setLogAction = const
-
-overLogAction :: HasLog env msg m => (LogAction m msg -> LogAction m msg) -> env -> env
-overLogAction over env = setLogAction (over $ getLogAction env) env
-{-# INLINE overLogAction #-}
+    overLogAction over = over
