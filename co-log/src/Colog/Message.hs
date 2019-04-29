de09de41c93a304ba0c12871dcbfb87de94ebe95
Dmitrii Kovanikov <dmitrii@holmusk.com>
2019-04-29 11:25:14 +0800

    Fix HLint

--- co-log/src/Colog/Message.hs
+++ co-log/src/Colog/Message.hs
@@ -3,7 +3,6 @@
 {-# LANGUAGE FlexibleContexts      #-}
 {-# LANGUAGE FlexibleInstances     #-}
 {-# LANGUAGE GADTs                 #-}
-{-# LANGUAGE KindSignatures        #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE OverloadedLabels      #-}
 {-# LANGUAGE TypeFamilies          #-}
