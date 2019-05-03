41953c17be20a938bbb7c7f5ac10d324b96fb522
Veronika Romashkina <vrom911@gmail.com>
2019-05-03 15:23:41 +0800

    [#101] Add logActionL lens, impove docs a bit (#105)
    * [#101] Add logActionL lens, impove docs a bit
    
    Resolves #101
    
    * Update co-log-core/src/Colog/Core/Class.hs
    
    Co-Authored-By: vrom911 <vrom911@gmail.com>
    
    * Fix after review
    
    * Fix hlint redundant lambda

--- co-log-core/src/Colog/Core/Class.hs
+++ co-log-core/src/Colog/Core/Class.hs
@@ -1,33 +1,87 @@
 {-# LANGUAGE FlexibleInstances     #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE Rank2Types            #-}
 
-{- | Provides type class for objects that has access to 'LogAction'.
+{- |
+Copyright:  (c) 2018-2019 Kowainik
+License:    MIT
+Maintainer: Kowainik <xrom.xkov@gmail.com>
+
+Provides type class for values that has access to 'LogAction'.
 -}
 
 module Colog.Core.Class
        ( HasLog (..)
+
+         -- * Lens
+         -- $lens
+       , Lens'
        ) where
 
 import Colog.Core.Action (LogAction)
 
-{- | This types class contains simple pair of getter-setter.
+
+-- to inline lens better
+{- HLINT ignore "Redundant lambda" -}
+
+{- | This types class contains simple pair of getter-setter and related
+functions.
+It also provides the useful lens 'logActionL' with the default implementation using type
+class methods. The default one could be easily overritten under your instances.
 
 TODO: laws
 -}
 class HasLog env msg m where
     {-# MINIMAL getLogAction, (setLogAction | overLogAction) #-}
     getLogAction :: env -> LogAction m msg
+
     setLogAction :: LogAction m msg -> env -> env
     setLogAction = overLogAction . const
     {-# INLINE setLogAction #-}
+
     overLogAction :: (LogAction m msg -> LogAction m msg) -> env -> env
     overLogAction f env = setLogAction (f $ getLogAction env) env
     {-# INLINE overLogAction #-}
 
+    logActionL :: Lens' env (LogAction m msg)
+    logActionL = lens getLogAction (flip setLogAction)
+    {-# INLINE logActionL #-}
+
 instance HasLog (LogAction m msg) msg m where
-    {-# INLINE getLogAction #-}
+    getLogAction :: LogAction m msg -> LogAction m msg
     getLogAction = id
-    {-# INLINE setLogAction #-}
+    {-# INLINE getLogAction #-}
+
+    setLogAction :: LogAction m msg -> LogAction m msg -> LogAction m msg
     setLogAction = const
-    {-# INLINE overLogAction #-}
+    {-# INLINE setLogAction #-}
+
+    overLogAction
+        :: (LogAction m msg -> LogAction m msg)
+        -> LogAction m msg
+        -> LogAction m msg
     overLogAction = id
+    {-# INLINE overLogAction #-}
+
+    logActionL :: Lens' (LogAction m msg) (LogAction m msg)
+    logActionL = \f s -> s <$ f s
+    {-# INLINE logActionL #-}
+
+----------------------------------------------------------------------------
+-- Lens
+----------------------------------------------------------------------------
+
+{- $lens
+To keep @co-log-core@ a lightweight library it was decided to introduce local
+'Lens'' type alias as it doesn't harm.
+-}
+
+{- | The monomorphic lenses which don't change the type of the container (or of
+the value inside).
+-}
+type Lens' s a = forall f. Functor f => (a -> f a) -> s -> f s
+
+-- | Creates 'Lens'' from the getter and setter.
+lens :: (s -> a) -> (s -> a -> s) -> Lens' s a
+lens getter setter = \f s -> setter s <$> f (getter s)
+{-# INLINE lens #-}
