ef32a37b3a5348c2afba8af18a44802529965ff4
vrom911 <vrom911@gmail.com>
2019-05-03 16:49:48 +0800

    Fix stack, improve cabal files

--- README.md
+++ README.md
@@ -3,15 +3,15 @@
 [![MPL-2.0 license](https://img.shields.io/badge/license-MPL--2.0-blue.svg)](https://github.com/kowainik/co-log/blob/master/LICENSE)
 [![Build status](https://secure.travis-ci.org/kowainik/co-log.svg)](https://travis-ci.org/kowainik/co-log)
 
-* `co-log`:
-  [![Hackage](https://img.shields.io/hackage/v/co-log.svg)](https://hackage.haskell.org/package/co-log)
-  [![Stackage LTS](http://stackage.org/package/co-log/badge/lts)](http://stackage.org/lts/package/co-log)
-  [![Stackage Nightly](http://stackage.org/package/co-log/badge/nightly)](http://stackage.org/nightly/package/co-log)
+`co-log`:
+[![Hackage](https://img.shields.io/hackage/v/co-log.svg)](https://hackage.haskell.org/package/co-log)
+[![Stackage LTS](http://stackage.org/package/co-log/badge/lts)](http://stackage.org/lts/package/co-log)
+[![Stackage Nightly](http://stackage.org/package/co-log/badge/nightly)](http://stackage.org/nightly/package/co-log)
 
-* `co-log-core`:
-  [![Hackage](https://img.shields.io/hackage/v/co-log-core.svg)](https://hackage.haskell.org/package/co-log-core)
-  [![Stackage LTS](http://stackage.org/package/co-log-core/badge/lts)](http://stackage.org/lts/package/co-log-core)
-  [![Stackage Nightly](http://stackage.org/package/co-log-core/badge/nightly)](http://stackage.org/nightly/package/co-log-core)
+`co-log-core`:
+[![Hackage](https://img.shields.io/hackage/v/co-log-core.svg)](https://hackage.haskell.org/package/co-log-core)
+[![Stackage LTS](http://stackage.org/package/co-log-core/badge/lts)](http://stackage.org/lts/package/co-log-core)
+[![Stackage Nightly](http://stackage.org/package/co-log-core/badge/nightly)](http://stackage.org/nightly/package/co-log-core)
 
 `co-log` is a composable and configurable logging framework. The idea of the approach is
 described in the following blog post:
--- co-log/test/Property.hs
+++ co-log/test/Property.hs
@@ -1,59 +1,63 @@
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE TemplateHaskell  #-}
+
 module Main where
 
-import System.IO (BufferMode(..), hSetBuffering, stdout, stderr)
+import System.Exit (exitFailure, exitSuccess)
+import System.IO (BufferMode (..), hSetBuffering, stderr, stdout)
 
-import Hedgehog
+import Hedgehog (MonadGen, Property, checkSequential, discover, forAll, property, (===))
 import qualified Hedgehog.Gen as Gen
 import qualified Hedgehog.Range as Range
 
-import Colog.Pure (PureLogger, runPureLog, logMessagePure)
-import Colog.Monad (usingLoggerT, logMsg)
 import Colog.Core (LogAction)
+import Colog.Monad (logMsg, usingLoggerT)
+import Colog.Pure (PureLogger, logMessagePure, runPureLog)
+
 
 data LogAST
     = LogMsg
     | AND LogAST LogAST deriving Show
 
 listLogMessages :: LogAST -> String -> [String]
-listLogMessages LogMsg msg = [msg]
+listLogMessages LogMsg msg    = [msg]
 listLogMessages (AND a b) msg = listLogMessages a msg ++ listLogMessages b msg
 
 processAST :: LogAction (PureLogger String) String -> String -> [String]
 processAST action msg = snd $ runPureLog $ usingLoggerT action $ logMsg msg
 
 toLoggerAction :: LogAST -> LogAction (PureLogger String) String
-toLoggerAction LogMsg = logMessagePure
+toLoggerAction LogMsg    = logMessagePure
 toLoggerAction (AND a b) = toLoggerAction a <> toLoggerAction b
 
-genAST :: MonadGen m => m  LogAST
-genAST = Gen.recursive  Gen.choice [ 
-    Gen.constant LogMsg
-  ] [
-    Gen.subterm2 genAST genAST AND
-  ]
+genAST :: MonadGen m => m LogAST
+genAST = Gen.recursive
+    Gen.choice
+    [ Gen.constant LogMsg ]
+    [ Gen.subterm2 genAST genAST AND ]
 
 prop_test_validate :: Property
 prop_test_validate = property $ do
-  msg <- forAll $ Gen.string (Range.constant 1 100) Gen.unicode
-  ast <- forAll genAST
-  processAST (toLoggerAction ast) msg === listLogMessages ast msg
+    msg <- forAll $ Gen.string (Range.constant 1 100) Gen.unicode
+    ast <- forAll genAST
+    processAST (toLoggerAction ast) msg === listLogMessages ast msg
 
 prop_test_assoc :: Property
 prop_test_assoc = property $ do
-  msg <- forAll $ Gen.string (Range.constant 1 100) Gen.unicode
-  x <- forAll genAST
-  y <- forAll genAST
-  z <- forAll genAST
-  let ax = toLoggerAction x
-  let ay = toLoggerAction y
-  let az = toLoggerAction z
-  processAST ((ax <> ay) <> az) msg === processAST (ax <> (ay <> az)) msg      
+    msg <- forAll $ Gen.string (Range.constant 1 100) Gen.unicode
+    x <- forAll genAST
+    y <- forAll genAST
+    z <- forAll genAST
+    let ax = toLoggerAction x
+    let ay = toLoggerAction y
+    let az = toLoggerAction z
+    processAST ((ax <> ay) <> az) msg === processAST (ax <> (ay <> az)) msg
 
 tests :: IO Bool
 tests = checkSequential $$(discover)
 
 main :: IO ()
 main = do
-  hSetBuffering stdout LineBuffering
-  hSetBuffering stderr LineBuffering
-  ifM tests exitSuccess exitFailure
+    hSetBuffering stdout LineBuffering
+    hSetBuffering stderr LineBuffering
+    tests >>= \p -> if p then exitSuccess else exitFailure
